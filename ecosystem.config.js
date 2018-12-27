518e6763130ec5030aa910db0ef72171751715a0
joshchen <joshchen@gmail.com>
2018-12-27 14:45:23 +0800

    aaa

---
+++ ecosystem.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  apps : [
+    {
+      name: "web1",
+      cwd:"/var/www/html/nuxt_test/web1",
+      script: "/var/www/html/nuxt_test/web1/node_modules/nuxt-start/bin/nuxt-start.js"
+    },
+    {
+      name: "web2",
+      cwd:"/var/www/html/nuxt_test/web2",
+      script: "/var/www/html/nuxt_test/web2/node_modules/nuxt-start/bin/nuxt-start.js"
+    },
+    {
+      name: "web3",
+      cwd:"/var/www/html/nuxt_test/web3",
+      script: "/var/www/html/nuxt_test/web3/node_modules/nuxt-start/bin/nuxt-start.js"
+    },
+    {
+      name: "web4",
+      cwd:"/var/www/html/nuxt_test/web4",
+      script: "/var/www/html/nuxt_test/web4/node_modules/nuxt-start/bin/nuxt-start.js"
+    },
+    {
+      name: "web5",
+      cwd:"/var/www/html/nuxt_test/web5",
+      script: "/var/www/html/nuxt_test/web5/node_modules/nuxt-start/bin/nuxt-start.js"
+    },
+    {
+      name: "web6",
+      cwd:"/var/www/html/nuxt_test/web6",
+      script: "/var/www/html/nuxt_test/web6/node_modules/nuxt-start/bin/nuxt-start.js"
+    },
+        {
+      name: "web7",
+      cwd:"/var/www/html/nuxt_test/web7",
+      script: "/var/www/html/nuxt_test/web7/node_modules/nuxt-start/bin/nuxt-start.js"
+    },
+    {
+      name: "web8",
+      cwd:"/var/www/html/nuxt_test/web8",
+      script: "/var/www/html/nuxt_test/web8/node_modules/nuxt-start/bin/nuxt-start.js"
+    },
+    {
+      name: "web9",
+      cwd:"/var/www/html/nuxt_test/web9",
+      script: "/var/www/html/nuxt_test/web9/node_modules/nuxt-start/bin/nuxt-start.js"
+    },
+    {
+      name: "web10",
+      cwd:"/var/www/html/nuxt_test/web10",
+      script: "/var/www/html/nuxt_test/web10/node_modules/nuxt-start/bin/nuxt-start.js"
+    }
+  ]
+}
